9e379eaa4f156bb4cf3cec1386db75120ed7f59e
Margo Boldar <mikhailmargo@yandex.ru>
2025-08-28 18:31:27 +0300

    Исправить CodeStyle

--- src/main/java/ru/practicum/shareit/constant/HttpHeaders.java
+++ src/main/java/ru/practicum/shareit/constant/HttpHeaders.java
@@ -2,5 +2,7 @@ package ru.practicum.shareit.constant;
 
 public class HttpHeaders {
     public static final String USER_ID_HEADER = "X-Sharer-User-Id";
-    private HttpHeaders() {}
+
+    private HttpHeaders() {
+    }
 }
